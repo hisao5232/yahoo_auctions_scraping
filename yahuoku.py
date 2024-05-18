02328db523fca4f47ffa900716cd31323edc4ad2
hisao <hisao52321983@gmail.com>
2024-05-18 22:22:20 +0900

    ページ送りまで確認

--- yahuoku.py
+++ yahuoku.py
@@ -6,9 +6,14 @@ import time
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 url='https://auctions.yahoo.co.jp/'
+time.sleep(2)
 
 def delete_ad():
     driver.find_element(By.CSS_SELECTOR,"div > a.Close-sc-uncojt.UslsY").click()
+
+def condition():
+    condition_new = driver.find_element(By.XPATH,"")
+
 
 def search(keyword):
     driver.get(url)
@@ -20,12 +25,19 @@ def search(keyword):
     elm.send_keys(keyword)
     #検索ボタンクリック
     driver.find_element(By.CSS_SELECTOR,"input#acHdSchBtn").click()
-    time.sleep(60)
+    time.sleep(3)
+
+    page_nation()
 
 def page_nation():
     while True:
         #次へクリック
-        driver.find_element(By.CSS_SELECTOR,"li.Pager__list.Pager__list--next > a").click()
+        try:
+            driver.find_element(By.CSS_SELECTOR,"li.Pager__list--next > a.Pager__link").click()
+            print("page")
+            time.sleep(3)
+        except:
+            break
 
 if __name__ == "__main__":
     search(keyword="Python")
